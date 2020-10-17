6c746dc21b3ed40923c934793aa302b556ba2121
DerejeKenno <dereje.kenno@gmail.com>
2020-10-17 09:57:57 -0500

    Initial Setup

---
+++ src/main/java/miu/cs545/goldendomonlinemarketing/GoldendomonlinemarketingApplication.java
@@ -0,0 +1,13 @@
+package miu.cs545.goldendomonlinemarketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldendomonlinemarketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldendomonlinemarketingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/miu/cs545/goldendomonlinemarketing/GoldendomonlinemarketingApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.cs545.goldendomonlinemarketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldendomonlinemarketingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
